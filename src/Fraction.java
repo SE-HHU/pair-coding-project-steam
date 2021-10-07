064968ef96e67131c4fa04f1b01593483bc9eba7
Guyuze <88260738+Guyuze@users.noreply.github.com>
2021-10-07 19:59:40 +0800

    Add files via upload

---
+++ src/Fraction.java
@@ -0,0 +1,10 @@
+public class Fraction{
+    int molecule;
+    int denominator;
+
+    public Fraction(int molecule, int denominator) {
+        this.molecule = molecule;
+        this.denominator = denominator;
+    }
+
+}
